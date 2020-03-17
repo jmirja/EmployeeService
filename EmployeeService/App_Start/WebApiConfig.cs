80ef66d74bbb2bce60e61bab447596846cd8e38c
jmirja <41453618+jmirja@users.noreply.github.com>
2020-03-17 23:26:09 +0600

    Add project files.

---
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace EmployeeService
+{
+    public static class WebApiConfig
+    {
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+          
+
+            config.Formatters.Add(new CustomJsonFormatter());
+
+            // this is always return json data when a reqest is made from browser but content-type: is misleading
+            // config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // indent json data
+            // config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            // camelcaseinstead of pascalcase
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // remove xml formatter, it will return json data when a reqest is made from browser or fiddler
+            // config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
